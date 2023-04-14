4af89c3f79fde9027f06c647fe3641c3a7f30cc3
Flominator <github.flominatorgd@gmail.com>
2023-04-14 12:25:30 +0200

    a6lte: add proper local path

--- a6lte-vendor.mk
+++ a6lte-vendor.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := $(call my-dir)
+LOCAL_PATH := vendor/samsung/a6lte
 
 
 PRODUCT_COPY_FILES += \
